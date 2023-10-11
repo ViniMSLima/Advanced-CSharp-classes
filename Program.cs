c3ee2f803698efd9a4c85b94f665f3313d649e56
ViniMSLima <121624623+ViniMSLima@users.noreply.github.com>
2023-10-11 13:52:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+MyApp.Current.Name = "Aplicacao_1";
+Console.WriteLine(MyApp.Current.Name);
+
+MyApp.Reset();
+
+Console.WriteLine(MyApp.Current.Name);
+
+MyApp.Current.Name = "Aplicacao_2";
+Console.WriteLine(MyApp.Current.Name);
+
+public class MyApp
+{
+    private MyApp() { }
+
+    public static MyApp Current { get; private set; } = new MyApp();
+
+    public string Name { get; set; }
+
+    public static void Reset()
+        => Current = new MyApp();
+}
